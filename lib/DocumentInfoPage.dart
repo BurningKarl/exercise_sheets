7d623aa13bb7f0910cced04a54c1da5e775a01b8
BurningKarl <karl.welzel@gmail.com>
2019-09-22 18:32:01 +0200

    Made DocumentInfoPage a StatefulWidget

--- lib/DocumentInfoPage.dart
+++ lib/DocumentInfoPage.dart
@@ -4,18 +4,30 @@ import 'package:provider/provider.dart';
 
 import 'DatabaseState.dart';
 
-class DocumentInfoPage extends StatelessWidget {
+
+class DocumentInfoPage extends StatefulWidget {
+  final int documentId;
+
+  DocumentInfoPage(this.documentId);
+
+  @override
+  DocumentInfoPageState createState() => DocumentInfoPageState(documentId);
+}
+
+class DocumentInfoPageState extends State<DocumentInfoPage> {
   final int documentId;
   final _formKey = GlobalKey<FormState>();
 
-  DocumentInfoPage(this.documentId);
+  DocumentInfoPageState(this.documentId);
 
   Widget buildContent(BuildContext context, DatabaseState database) {
     return Form(
       key: _formKey,
       child: Scrollbar(
         child: ListView(
-          children: <Widget>[TextFormField()],
+          children: <Widget>[TextFormField(
+            autofocus: true,
+          )],
         ),
       ),
     );
