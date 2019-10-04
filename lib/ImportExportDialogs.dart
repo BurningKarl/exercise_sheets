f25a7404a5c296936541fbf1357c6faf554f33c2
BurningKarl <karl.welzel@gmail.com>
2019-10-05 00:41:01 +0200

    Moved import and export dialogs to a separate file

---
+++ lib/ImportExportDialogs.dart
@@ -0,0 +1,139 @@
+import 'dart:io';
+
+import 'package:exercise_sheets/DatabaseState.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
+
+enum ExportOption { EXPORT, IMPORT, CANCEL }
+
+class ImportExportDialogs {
+  static Future<void> handleExport(
+      BuildContext context, DatabaseState databaseState) async {
+    Directory baseDirectory = await getExternalStorageDirectory();
+    var file = File(baseDirectory.path + '/exercise_sheets.json');
+    print(file.path);
+
+    databaseState.exportToFile(file).then((_) {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Export successful'),
+              content: Text('Your exercise sheets were succesfully exported to '
+                  '"${file.path}".'),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('OK'),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+              ],
+            );
+          });
+    }).catchError((error) {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Export failed'),
+              content:
+              Text('Your exercise sheets could not be exported: $error'),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('OK'),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+              ],
+            );
+          });
+    });
+  }
+
+  static Future<void> handleImport(
+      BuildContext context, DatabaseState databaseState) async {
+    File file = File(await FilePicker.getFilePath(
+        type: FileType.ANY, fileExtension: 'json'));
+
+    databaseState.importFromFile(file).then((_) {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Import successful'),
+              content: Text('Your exercise sheets were succesfully imported '
+                  'from "${file.path}".'),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('OK'),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+              ],
+            );
+          });
+    }).catchError((error) {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text('Import failed'),
+              content:
+              Text('Your exercise sheets could not be imported: $error'),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('OK'),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+              ],
+            );
+          });
+    });
+  }
+
+  static Future<void> handleImportExport(
+      BuildContext context, DatabaseState databaseState) async {
+    var option = await showDialog<ExportOption>(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Import or export'),
+            content: Text(
+                'You can import and export your websites and corresponding '
+                    'exercise sheets inluding their points, but without the actual '
+                    'PDF files.'),
+            actions: <Widget>[
+              FlatButton(
+                child: Text('CANCEL'),
+                onPressed: () {
+                  Navigator.of(context).pop(ExportOption.CANCEL);
+                },
+              ),
+              FlatButton(
+                child: Text('IMPORT'),
+                onPressed: () {
+                  Navigator.of(context).pop(ExportOption.IMPORT);
+                },
+              ),
+              FlatButton(
+                child: Text('EXPORT'),
+                onPressed: () {
+                  Navigator.of(context).pop(ExportOption.EXPORT);
+                },
+              ),
+            ],
+          );
+        });
+
+    switch (option) {
+      case ExportOption.EXPORT:
+        await handleExport(context, databaseState);
+        break;
+      case ExportOption.IMPORT:
+        await handleImport(context, databaseState);
+        break;
+      case ExportOption.CANCEL:
+        break;
+    }
+  }
+
+}
--- lib/WebsiteSelectionPage.dart
+++ lib/WebsiteSelectionPage.dart
@@ -1,17 +1,12 @@
-import 'dart:io';
-
 import 'package:exercise_sheets/DatabaseState.dart';
 import 'package:exercise_sheets/DocumentSelectionPage.dart';
+import 'package:exercise_sheets/ImportExportDialogs.dart';
 import 'package:exercise_sheets/WebsiteInfoPage.dart';
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 
 import 'DatabaseState.dart';
-
-enum ExportOption { EXPORT, IMPORT, CANCEL }
 
 class WebsiteSelectionPage extends StatefulWidget {
   @override
@@ -40,133 +35,6 @@ class WebsiteSelectionPageState extends State<WebsiteSelectionPage> {
     });
   }
 
-  Future<void> handleExport(
-      BuildContext context, DatabaseState databaseState) async {
-    Directory baseDirectory = await getExternalStorageDirectory();
-    var file = File(baseDirectory.path + '/exercise_sheets.json');
-    print(file.path);
-
-    databaseState.exportToFile(file).then((_) {
-      showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: Text('Export successful'),
-              content: Text('Your exercise sheets were succesfully exported to '
-                  '"${file.path}".'),
-              actions: <Widget>[
-                FlatButton(
-                  child: Text('OK'),
-                  onPressed: () => Navigator.of(context).pop(),
-                ),
-              ],
-            );
-          });
-    }).catchError((error) {
-      showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: Text('Export failed'),
-              content:
-                  Text('Your exercise sheets could not be exported: $error'),
-              actions: <Widget>[
-                FlatButton(
-                  child: Text('OK'),
-                  onPressed: () => Navigator.of(context).pop(),
-                ),
-              ],
-            );
-          });
-    });
-  }
-
-  Future<void> handleImport(
-      BuildContext context, DatabaseState databaseState) async {
-    File file = File(await FilePicker.getFilePath(
-        type: FileType.ANY, fileExtension: 'json'));
-
-    databaseState.importFromFile(file).then((_) {
-      showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: Text('Import successful'),
-              content: Text('Your exercise sheets were succesfully imported '
-                  'from "${file.path}".'),
-              actions: <Widget>[
-                FlatButton(
-                  child: Text('OK'),
-                  onPressed: () => Navigator.of(context).pop(),
-                ),
-              ],
-            );
-          });
-    }).catchError((error) {
-      showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            return AlertDialog(
-              title: Text('Import failed'),
-              content:
-                  Text('Your exercise sheets could not be imported: $error'),
-              actions: <Widget>[
-                FlatButton(
-                  child: Text('OK'),
-                  onPressed: () => Navigator.of(context).pop(),
-                ),
-              ],
-            );
-          });
-    });
-  }
-
-  Future<void> handleImportExport(
-      BuildContext context, DatabaseState databaseState) async {
-    var option = await showDialog<ExportOption>(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text('Import or export'),
-            content: Text(
-                'You can import and export your websites and corresponding '
-                'exercise sheets inluding their points, but without the actual '
-                'PDF files.'),
-            actions: <Widget>[
-              FlatButton(
-                child: Text('CANCEL'),
-                onPressed: () {
-                  Navigator.of(context).pop(ExportOption.CANCEL);
-                },
-              ),
-              FlatButton(
-                child: Text('IMPORT'),
-                onPressed: () {
-                  Navigator.of(context).pop(ExportOption.IMPORT);
-                },
-              ),
-              FlatButton(
-                child: Text('EXPORT'),
-                onPressed: () {
-                  Navigator.of(context).pop(ExportOption.EXPORT);
-                },
-              ),
-            ],
-          );
-        });
-
-    switch (option) {
-      case ExportOption.EXPORT:
-        await handleExport(context, databaseState);
-        break;
-      case ExportOption.IMPORT:
-        await handleImport(context, databaseState);
-        break;
-      case ExportOption.CANCEL:
-        break;
-    }
-  }
-
   Widget buildWebsiteItem(
       BuildContext context, int index, DatabaseState databaseState) {
     Map<String, dynamic> website = databaseState.websites[index];
@@ -180,10 +48,6 @@ class WebsiteSelectionPageState extends State<WebsiteSelectionPage> {
           '/${pointsFormat.format(stats['maximum'])} '
           ' ~ ${averageFormat.format(stats['achieved'] / stats['maximum'])}';
     }
-
-    // TODO: Consider adding the ability to select websites
-    // After one or more websites are selected, a delete button can
-    // be shown
 
     return Dismissible(
       key: Key(website['id'].toString()),
@@ -313,7 +177,8 @@ class WebsiteSelectionPageState extends State<WebsiteSelectionPage> {
             actions: <Widget>[
               IconButton(
                 icon: Icon(Icons.import_export),
-                onPressed: () => handleImportExport(context, databaseState),
+                onPressed: () => ImportExportDialogs.handleImportExport(
+                    context, databaseState),
               )
             ],
           );
